c48fd1234b45c22cac2a418ee3fa4dd81b7e4095
Cody Winchester <cjwinchester@gmail.com>
2017-04-11 02:37:03 -0500

    add auth

--- achewood.py
+++ achewood.py
@@ -1,23 +1,29 @@
 from os import environ
 import random
-from flask import Flask, jsonify
+import json
+from flask import Flask, jsonify, request, abort
 from strips import strips as strip_ls
 
 app = Flask(__name__)
 
+token = environ.get('SLASH_TOKEN', None)
+
 
 @app.route('/post', methods=['POST'])
 def post():
-    url_stub = 'http://achewood.com/comic.php?date='
-    random_day = random.randint(0, len(strip_ls))
-    strip = strip_ls[random_day]
-    response = {
-        "response_type": "in_channel",
-        "text": url_stub + strip
-    }
-    return jsonify(response)
+    if token and json.loads(request.data)['token'] == token:
+        url_stub = 'http://achewood.com/comic.php?date='
+        random_day = random.randint(0, len(strip_ls))
+        strip = strip_ls[random_day]
+        response = {
+            "response_type": "in_channel",
+            "text": url_stub + strip
+        }
+        return jsonify(response)
+    else:
+        abort(403)
 
 
 if __name__ == '__main__':
     port = int(environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
